05151a6064f0897526e5e1b57896c583790bb2fd
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-13 18:15:19 +0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  providers: [DataService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-app';
+  photos: any[] = [];
+
+  constructor(private DataService: DataService) {
+    this.DataService.getPhotos().subscribe((photos: any) => {
+      this.photos = photos;
+    });
+  }
 }
